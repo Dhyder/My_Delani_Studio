1417982efa40e60aee1026524accb73cdea2e829
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-09-13 20:49:08 +0300

    first4functions

---
+++ jquery/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#pdesign").click(function(){
+      $("#pdesign").hide();
+      $("#pdesign2").show();
+  });
+  $("#pdesign2").click(function(){
+    $("#pdesign2").slideUp();
+    $("#pdesign").slideDown();
+  });
+  });
+  
+  $(document).ready(function(){
+    $("#pdevelopment").click(function(){
+      $("#pdevelopment").hide();
+      $("#pdevelopment2").show();
+  });
+  $("#pdevelopment2").click(function(){
+    $("#pdevelopment2").slideUp();
+    $("#pdevelopment").slideDown();
+  });
+  });
+  $(document).ready(function(){
+    $("#product").click(function(){
+      $("#product").hide();
+      $("#product2").show();
+  });
+  $("#product2").click(function(){
+    $("#product2").slideUp();
+    $("#product").slideDown();
+  });
+  });
+  
+  
