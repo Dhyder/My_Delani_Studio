94f3adfe378a026b622ad52cc73723627a8a5aaf
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-09-14 12:16:00 +0300

    jquery_update1173css&&html

--- jquery/script.js
+++ jquery/script.js
@@ -29,56 +29,155 @@ $(document).ready(function(){
     $("#product").slideDown();
   });
   });
-  
-
-
+///////
+  $(document).ready(function(){
+    $("#mouse_click_1").mouseover(function(){
+      $("#mouse_click_1").slideUp(1000);
+    }).mouseout(function(){
+      $("#mouse_click_1").slideDown(1500);
+    });
+  });
+  $(document).ready(function(){
+    $("#dribble_1").mouseover(function(){
+      $("#dribble_1").slideUp(500);
+    }).mouseout(function(){
+      $("#dribble_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#facebook_1").mouseover(function(){
+      $("#facebook_1").slideUp(500);
+    }).mouseout(function(){
+      $("#facebook_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#g_plus_1").mouseover(function(){
+      $("#g_plus_1").slideUp(500);
+    }).mouseout(function(){
+      $("#g_plus_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#skype_1").mouseover(function(){
+      $("#skype_1").slideUp(500);
+    }).mouseout(function(){
+      $("#skype_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#behance_1").mouseover(function(){
+      $("#behance_1").slideUp(500);
+    }).mouseout(function(){
+      $("#behance_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#twitter_1").mouseover(function(){
+      $("#twitter_1").slideUp(500);
+    }).mouseout(function(){
+      $("#twitter_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#pinterest_1").mouseover(function(){
+      $("#pinterest_1").slideUp(500);
+    }).mouseout(function(){
+      $("#pinterest_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#flickr_1").mouseover(function(){
+      $("#flickr_1").slideUp(500);
+    }).mouseout(function(){
+      $("#flickr_1").slideDown(500);
+    });
+  });
+  $(document).ready(function(){
+    $("#stumble_upon_1").mouseover(function(){
+      $("#stumble_upon_1").slideUp(500);
+    }).mouseout(function(){
+      $("#stumble_upon_1").slideDown(500);
+    });
+  });
+//////
   $(document).ready(function(){
     $("#gray_background").mouseover(function(){
-      $("#white_front0").show();
+      $("#white_front0").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front0").hide();
+        $("#white_front0").slideUp(5000);
     });
     $("#ontario").mouseover(function(){
-      $("#white_front1").show();
+      $("#white_front1").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front1").hide();
+        $("#white_front1").slideUp(5000);
     });
  });
  $(document).ready(function(){
     $("#orange_brown").mouseover(function(){
-      $("#white_front2").show();
+      $("#white_front2").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front2").hide();
+        $("#white_front2").slideUp(5000);
     });
     $("#jim_carey_").mouseover(function(){
-      $("#white_front3").show();
+      $("#white_front3").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front3").hide();
+        $("#white_front3").slideUp(5000);
     });
  });
  $(document).ready(function(){
     $("#work5_1").mouseover(function(){
-      $("#white_front4").show();
+      $("#white_front4").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front4").hide();
+        $("#white_front4").slideUp(5000);
     });
     $("#work6_1").mouseover(function(){
-      $("#white_front5").show();
+      $("#white_front5").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front5").hide();
+        $("#white_front5").slideUp(5000);
     });
     $("#work7_1").mouseover(function(){
-      $("#white_front6").show();
+      $("#white_front6").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front6").hide();
+        $("#white_front6").slideUp(5000);
     });
     $("#work8_1").mouseover(function(){
-      $("#white_front7").show();
+      $("#white_front7").slideDown(3000);
     }).mouseout(function(){
-        $("#white_front7").hide();
+        $("#white_front7").slideUp(5000);
     });
  });
- 
-function popup(){
-    swal("Oops!", "Message not Sent", "error");
-    } 
+
+//////////////////////////////////////
+
+    $("form#form").on('submit',function(event){
+      event.preventDefault();
+      let name = $("input#name_box").val();
+      let email = $("input#email___").val();
+      let message = $("textarea#your_message_here___").val();
+
+      if ($("input#name_box").val() && $("input#email___").val()){
+          swal("Thankyou!" + name + "", "Message Sent", "success");
+          alert("Thankyou for your feedback" + "", name)
+      }
+  });
+
+////////////////////////////////////////
+  function nameValidation(){
+    let name = document.getElementById("name_box").value;
+    if(name == "")
+    swal("Please input field"," ", "error");
+    return false;
+} 
+function emailValidation(){
+    let email = document.getElementById("email___").value;
+    if(email == "")
+    swal("Please input field"," ","error");
+    return false;
+} 
+function textValidation(){
+    let text = document.getElementById("your_message_here___").value;
+    if(text == "")
+    swal("Please input field"," ","error");
+    return false;
+} 
